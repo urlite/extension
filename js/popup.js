5537108d8a4dbce1efcf8ef7468ee12432b2d5a3
Mederic <medericnetto@gmail.com>
2020-07-31 19:42:50 +0200

    First version

---
+++ js/popup.js
@@ -0,0 +1,30 @@
+let finalUrl;
+
+var clipboard = new ClipboardJS('.btn');
+
+chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+}, tabs => {
+    let currentUrl = tabs[0].url;
+    document.getElementById('url').value = currentUrl;
+});
+
+demo.onclick = function (element) {
+    let myUrl = document.getElementById("url").value;
+
+    fetch(config.MY_URL, {
+            method: 'post',
+            body: JSON.stringify({
+                url: myUrl
+            })
+        }).then(function (response) {
+            return response.json();
+        }).then(function (result) {
+            finalUrl = result;
+            document.getElementById('result').value = finalUrl;
+        })
+        .catch(function (error) {
+            console.log('Request failed', error);
+        });
+};
